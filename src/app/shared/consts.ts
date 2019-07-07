577927cac313d74d8641daea3467058d46bcb668
jb <jbtwitt@outlook.com>
2019-07-07 10:25:16 -0400

    consts

--- src/app/shared/consts.ts
+++ src/app/shared/consts.ts
@@ -8,34 +8,3 @@ export const audios = [
     new Audio("assets/audios/five.m4a"),
 ];
 export const v1234 = new Audio(`${audioPath}/v1234.m4a`);
-
-import { Observable } from 'rxjs';
-
-import { AlternateTempoSettingModel } from '../models/tempo-settings.model';
-export const alternateBeat = (settings: AlternateTempoSettingModel): Observable<any> => {
-  return new Observable(subscriber => {
-    let t = 0;
-    // v1234.play();
-    subscriber.next('go');
-    const intervalId = setInterval(() => {
-      // if (t < 4) audios[t].play();
-      switch (++t) {
-        case settings.startBeats:
-          // v1234.play();
-          subscriber.next('stop');
-          break;
-        case settings.totalBeats:
-          // audios[0].play();
-          subscriber.next('go');
-          t = 0;
-      }
-      console.log('' + settings.totalBeats + ', ' + t);
-    }, settings.intervalNumber);
-
-    // return unsubscribe for subsciber to dispose
-    return () => {
-      console.log('unsubscribe');
-      clearInterval(intervalId);
-    }
-  });
-}
