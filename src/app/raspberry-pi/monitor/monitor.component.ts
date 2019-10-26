c6ea8d4e3033eb52c1fd9592fd3d4ced91d346a8
jb <jbtwitt@outlook.com>
2019-10-26 15:11:49 -0400

    monitor adjust image size

--- src/app/raspberry-pi/monitor/monitor.component.ts
+++ src/app/raspberry-pi/monitor/monitor.component.ts
@@ -1,29 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
-const pi1Url = "http://192.168.0.118:5000/snapshot"
-const pi2Url = "http://192.168.0.119:5000/snapshot"
+const PiUrls = [
+  "http://192.168.0.118:5000/snapshot",
+  "http://192.168.0.119:5000/snapshot",
+]
 
 @Component({
   selector: 'app-monitor',
   templateUrl: './monitor.component.html',
   styleUrls: ['./monitor.component.sass']
 })
-export class MonitorComponent implements OnInit {
-  timestamp: Date = new Date;
-  seconds: number = 2;
+export class MonitorComponent implements OnInit, OnDestroy {
+  piImgs = PiUrls.slice()
 
-  piImgs = [pi1Url, pi2Url]
+  timestamp: Date = new Date;
+  seconds: number = 1.5;
+
+  intervalId: any
+  // count: number = 0
+  // maxCount: number = 1000;
 
   constructor() { }
 
   ngOnInit() {
-    const intervalId = setInterval(() => {
+    this.intervalId = setInterval(() => {
       this.timestamp = new Date;
-      this.piImgs = [
-        pi1Url + '?' + this.timestamp.getSeconds(),
-        pi2Url + '?' + this.timestamp.getSeconds()
-      ]
+      for (let i = 0; i < this.piImgs.length; i++) {
+        this.piImgs[i] = PiUrls[i] + '?' + this.timestamp.getSeconds()
+      }
+      // if (++this.count > this.maxCount) {
+      //   clearInterval(this.intervalId)
+      // }
     }, this.seconds * 1000);
   }
 
+  ngOnDestroy() {
+    console.log('destory')
+    clearInterval(this.intervalId)
+  }
 }
