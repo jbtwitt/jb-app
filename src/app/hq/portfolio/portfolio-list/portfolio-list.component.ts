b50cfb3e41fa85fa1197ab718b4355cab8c28bfc
unknown <jbtwitt@outlook.com>
2020-04-18 11:47:04 -0400

    add group by

--- src/app/hq/portfolio/portfolio-list/portfolio-list.component.ts
+++ src/app/hq/portfolio/portfolio-list/portfolio-list.component.ts
@@ -10,10 +10,14 @@ import { UiService } from 'src/app/services/ui.service';
 })
 export class PortfolioListComponent implements OnInit, OnChanges {
   @Input() portfolio: any[] = [];
-  displayedColumns: string[];
-  moreDisplay =  ['gainLoss', 'gainLossP'];
   dataSource: MatTableDataSource<any[]>;
   @ViewChild(MatSort, {static: true}) sort: MatSort;
+  displayedColumns = [
+    "broker", "ticker", "shares",
+    "buyPrice", "buyDate",
+    "soldPrice", "soldDate",
+    'gainLoss', 'gainLossP',
+  ];
 
   constructor(
     public uiService: UiService,
@@ -25,9 +29,9 @@ export class PortfolioListComponent implements OnInit, OnChanges {
   ngOnChanges() {
     console.log(this.portfolio);
     if (this.portfolio && this.portfolio.length > 0) {
-      const cols = Object.keys(this.portfolio[0]).filter(k => k !== 'broker');
+      // const cols = Object.keys(this.portfolio[0]).filter(k => k !== 'broker');
       // use spread syntax instead of using array push
-      this.displayedColumns = ['broker', ...cols, ...this.moreDisplay];
+      // this.displayedColumns = ['broker', ...cols, ...this.moreDisplay];
       this.dataSource = new MatTableDataSource(this.portfolio);
       this.dataSource.sort = this.sort;
     }
--- src/app/hq/portfolio/portfolio-main/portfolio-main.component.ts
+++ src/app/hq/portfolio/portfolio-main/portfolio-main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import _ from 'lodash';
 import { DataService } from 'src/app/services/data.service';
 
 @Component({
@@ -10,6 +11,7 @@ export class PortfolioMainComponent implements OnInit {
   portfolio: any[];
   portfolioOpen: any[];
   portfolioClose: any[];
+  groupOpen: any[];
 
   constructor(
     private dataService: DataService,
@@ -18,8 +20,15 @@ export class PortfolioMainComponent implements OnInit {
   ngOnInit() {
     this.dataService.getAssetCsvData('portfolio.csv').subscribe(arr => {
       this.portfolio = arr;
+      // string to number
+      arr.forEach(item => {
+        item.shares = + item.shares;
+        item.buyCost = +item.shares * item.buyPrice;
+      });
       this.portfolioOpen = arr.filter(p => p.soldDate === '');
       this.portfolioClose = arr.filter(p => p.soldDate !== '');
+
+      this.groupOpen = this.groupByTicker(this.portfolioOpen);
 
       // use destructuring assignment
       // to fill in latest close price
@@ -29,8 +38,42 @@ export class PortfolioMainComponent implements OnInit {
           const curInfo = data.filter(d => d.ticker === p.ticker)[0];
           [p.soldPrice, p.soldDate] = [curInfo.cClose, curInfo.cDate];
         });
+        this.groupOpen.forEach(p => {
+          const curInfo = data.filter(d => d.ticker === p.ticker)[0];
+          [p.soldPrice, p.soldDate] = [curInfo.cClose, curInfo.cDate];
+        });
       });
     })
+
   }
 
+  groupByTicker_(data) {
+    // const groupBy = _.chain(data)
+    //     .groupBy("ticker")
+    //     // .map((value, key) => )
+    //     .value();
+  }
+  groupByTicker(data) {
+    // let copy = Object.assign({}, data); // assign doesn't do deep cloning
+    const copy = JSON.parse(JSON.stringify(data));
+    const groupBy = copy.reduce((acc, row) => {
+      const key = acc[row.ticker];
+      if (key) {
+        key.shares += row.shares;
+        key.buyCost += row.buyCost;
+        key.buyPrice = key.buyCost / key.shares;
+      } else {
+        acc[row.ticker] = row;
+        delete row.ticker;
+      }
+      return acc;
+    }, {});
+    const result = [];
+    Object.keys(groupBy).forEach(key => {
+      groupBy[key] = {ticker: key, ...groupBy[key]};
+      result.push(groupBy[key]);
+    })
+    console.log(result)
+    return result;
+  }
 }
