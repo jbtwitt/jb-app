88838488db929b3582de4c61363a340833df5fa8
unknown <jbtwitt@outlook.com>
2020-04-22 18:14:49 -0400

    group portfolio

--- src/app/hq/portfolio/portfolio-main/portfolio-main.component.ts
+++ src/app/hq/portfolio/portfolio-main/portfolio-main.component.ts
@@ -12,34 +12,29 @@ export class PortfolioMainComponent implements OnInit {
   portfolioOpen: any[];
   portfolioClose: any[];
   portfolioTest: any[];
-  groupTestPortfolio: any[];
-  groupPortfolioOpen: any[];
-
-  groupTicker: string;
 
   constructor(
     private dataService: DataService,
   ) { }
 
   ngOnInit() {
-    this.dataService.getAssetCsvData('portfolio.csv').subscribe(arr => {
-      this.portfolio = arr;
+    this.dataService.getAssetCsvData('portfolio.csv').subscribe(csv => {
+      this.portfolio = csv;
 
-      arr.forEach(item => {
+      csv.forEach(item => {
         item.buyCost = item.shares * item.buyPrice;
       });
 
-      this.portfolioClose = arr.filter(p => p.soldDate !== '');
+      this.portfolioClose = csv.filter(p => p.soldDate !== '');
 
       this.dataService.getAssetCsvData("hqcsv/hqstat-200.csv").subscribe(data => {
 
-        const opens = arr.filter(
+        const opens = csv.filter(
           p => p.soldDate === ''
         );
 
         // use destructuring assignment
-        // to fill in close price
-        // in the place of sold date
+        // to fill in close price in the place of sold date
         opens.forEach(p => {
           const curInfo = data.filter(d => d.ticker === p.ticker)[0];
           [p.soldPrice, p.soldDate, p.csvPath] =
@@ -52,62 +47,7 @@ export class PortfolioMainComponent implements OnInit {
           p => p.broker !== 'Test *'
         );
       });
-
-
-      // this.groupTestPortfolio = arr.filter(p => p.soldDate === '');
-      // this.groupPortfolioOpen = this.createGroupByTicker(this.groupTestPortfolio);
-
-      // this.dataService.getAssetCsvData("hqcsv/hqstat-200.csv").subscribe(data => {
-      //   // this.portfolioOpen.forEach(p => {
-      //   //   const curInfo = data.filter(d => d.ticker === p.ticker)[0];
-      //   //   [p.soldPrice, p.soldDate, p.csvPath] = [+curInfo.cClose, curInfo.cDate, curInfo.csvPath];
-      //   // });
-      //   this.groupPortfolioOpen.forEach(p => {
-      //     const curInfo = data.filter(d => d.ticker === p.ticker)[0];
-      //     [p.soldPrice, p.soldDate, p.csvPath] = [curInfo.cClose, curInfo.cDate, curInfo.csvPath];
-      //     // find group ticker if any, to show detailed list
-      //     if (!p.buyDate) {
-      //       this.groupTicker = p.ticker;
-      //     }
-      //   });
-      // });
     });
   }
 
-  groupTickerSelected(ticker) {
-    this.groupTicker = ticker;
-  }
-  get groupList() {
-    return this.groupTestPortfolio.filter(p => p.ticker === this.groupTicker);
-  }
-  // groupByTicker_(data) {
-    // const groupBy = _.chain(data)
-    //     .groupBy("ticker")
-    //     // .map((value, key) => )
-    //     .value();
-  // }
-  createGroupByTicker(data) {
-    // let copy = Object.assign({}, data); // assign doesn't do deep cloning
-    const copy = JSON.parse(JSON.stringify(data));
-    const groupBy = copy.reduce((acc, row) => {
-      const key = acc[row.ticker];
-      if (key) {
-        key.shares += row.shares;
-        key.buyCost += row.buyCost;
-        key.buyPrice = key.buyCost / key.shares;
-        key.buyDate = '';
-      } else {
-        acc[row.ticker] = row;
-        delete row.ticker;
-      }
-      return acc;
-    }, {});
-    const result = [];
-    Object.keys(groupBy).forEach(key => {
-      groupBy[key] = {ticker: key, ...groupBy[key]};
-      result.push(groupBy[key]);
-    })
-    // console.log(result)
-    return result;
-  }
 }
