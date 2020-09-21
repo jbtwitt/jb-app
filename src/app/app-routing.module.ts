72a28465df05e9c591d60bb30cd471b3c189d065
unknown <jbtwitt@outlook.com>
2020-09-21 19:06:32 -0400

    home page: monitor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { MonitorComponent } from './raspberry-pi/monitor/monitor.component';
 
 const routes: Routes = [
   // { path: '', redirectTo: '/at-collection', pathMatch: 'full' }
-  // { path: '', redirectTo: '/bookmarks', pathMatch: 'full' },
-  { path: '', redirectTo: '/hqhl', pathMatch: 'full' },
+  { path: '', redirectTo: '/monitor', pathMatch: 'full' },
+  // { path: '', redirectTo: '/hqhl', pathMatch: 'full' },
   // { path: '', component: MonitorComponent }
 ];
 
