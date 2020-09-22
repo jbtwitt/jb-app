9deb9d5bfc9dba7f8631fd21f49bd0af47ec6390
unknown <jbtwitt@outlook.com>
2020-09-22 09:53:17 -0400

    app menu layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnDestroy {
   mobileQuery: MediaQueryList;
