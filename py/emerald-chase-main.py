61307bd887b95f1d7c2b42e242daab437eb689c2
unknown <jbtwitt@outlook.com>
2020-10-12 12:28:12 -0400

    separate load img & yolo

--- py/emerald-chase-main.py
+++ py/emerald-chase-main.py
@@ -1,6 +1,5 @@
 import json
 import cv2
-from Yolo import Yolo
 
 import numpy as np
 from urllib.request import urlopen
@@ -12,21 +11,28 @@ def httpGetImg(imgUrl):
   img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
   return img
 
-def run(modelConf, piUrl):
-  yoloNet = Yolo(modelConf)
-  img = httpGetImg(piUrl)
-  img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
-  # cv2.imshow("PiUrl", img)
-  # cv2.waitKey(0)
-  objs = yoloNet.findDetectedObjects(img)
-  print(objs)
-  if objs is not None:
-    yoloNet.drawDetectedObjects("title", img, objs)
+def loadImgs():
+  imgs = []
+  pis = json.load(open("../src/assets/pi-addr.json"))
+  for pi in pis:
+    print(pi)
+    img = httpGetImg(pi['url'])
+    if pi['rotate']:
+      img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
+    imgs.append(img)
+  return imgs
+
+from Yolo import Yolo
+def run(modelPath, imgs):
+  yoloNet = Yolo(modelPath)
+  for img in imgs:
+    objs = yoloNet.findDetectedObjects(img)
+    print(objs)
+    if objs is not None:
+      yoloNet.drawDetectedObjects("title", img, objs)
 
 if __name__ == '__main__':
   jbConf = json.load(open("jbconf.json"))
+  modelPath = jbConf["models"]["yolov3"]
   # print(jbConf)
-  # piAddrs = json.load(open("../src/assets/pi-addr.json"))
-  # print(piAddrs)
-  pi115Url = "http://192.168.0.115:5000/pi-monitor"
-  run(jbConf["models"]["yolov3"], pi115Url)
+  run(modelPath, loadImgs())
