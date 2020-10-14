efa032121a46491dac588ebeb612deb1c8e37133
unknown <jbtwitt@outlook.com>
2020-10-14 18:10:41 -0400

    main command

--- py/emerald-chase-main.py
+++ py/emerald-chase-main.py
@@ -1,73 +1,83 @@
 import cv2
 import json
-import numpy as np
-
-from emerald_chase_util import httpGetImg, getImgInfo
-def loadImgs(pis):
-  imgs = []
-  for pi in pis:
-    print(pi)
-    try:
-      img = httpGetImg(pi['url'])
-      if pi['rotate']:
-        img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
-      imgs.append(img)
-    except Exception as e:
-      print(e)
-  return imgs
-
-from Yolo import Yolo
-def run(yoloNet, imgs):
-  for img in imgs:
-    objs = yoloNet.findDetectedObjects(img)
-    print(objs)
-    if objs is not None:
-      yoloNet.drawDetectedObjects("title", img, objs)
-
-
 import time
-from Yolo import DiffYolo, DIFFDECODES
-def runMovement(yoloNet, pi, loop=3):
-  detect = DiffYolo(trainedImageSize=(608, 608))
-  for i in range(loop):
-    try:
-      imgInfo = getImgInfo(pi)
-      ret = detect.run(yoloNet, imgInfo)
-      foundObjs = imgInfo['foundObjs']
-      print(i, 'imgInfo foundObjs -> ', foundObjs)
-      if ret is not None:
-        print('*****', ret, DIFFDECODES[ret])
-        yoloNet.drawDetectedObjects('Diff', imgInfo['img'], imgInfo['foundObjs'])
-      time.sleep(.5)
-    except Exception as e:
-      print('runMovement', e)
+import numpy as np
+from Yolo import Yolo, DiffYolo, DIFFDECODES
+from emerald_chase_util import httpGetImg, getImgInfo, PiCamCacheRepo
 
-from emerald_chase_util import PiCamCacheRepo
-def runSearch(yoloNet, piIp, day, pattern, rotate=False, classId=0):
-  count = 0
-  piRepo = PiCamCacheRepo(piIp, day=0, pattern=pattern)
-  while(True):
-    imgInfo = piRepo.getImgInfo(rotate=True)
-    if (imgInfo is None):
-      break
-    objs = yoloNet.findDetectedObjects(imgInfo['img'])
-    if objs is not None:
-      print(imgInfo['timestamp'], 'imgInfo foundObjs -> ', objs)
-      for obj in objs:
-        if obj['classId'] == classId:
-          yoloNet.drawDetectedObjects(imgInfo['timestamp'], imgInfo['img'], objs)
-    count = count + 1
-  print('Total', count, 'searched')
+class HomePiApp:
+  def __init__(self):
+    self.pis = json.load(open("../src/assets/pi-addr.json"))
+    jbConf = json.load(open("jbconf.json"))
+    modelPath = jbConf["models"]["yolov3"]
+    self.yoloNet = Yolo(modelPath)
+
+  def getPiImgs(self):
+    imgs = []
+    for pi in self.pis:
+      try:
+        img = httpGetImg(pi['url'])
+        if pi['rotate']:
+          img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
+        imgs.append(img)
+      except Exception as e:
+        print(e)
+    return imgs
+
+  def runTakeaLook(self):
+    for img in self.getPiImgs():
+      objs = self.yoloNet.findDetectedObjects(img)
+      print(objs)
+      if objs is not None:
+        self.yoloNet.drawDetectedObjects('PiImg', img, objs)
+
+  def runCamMovement(self, piId=0, loop=3):
+    pi = self.pis[piId]
+    detect = DiffYolo(trainedImageSize=(608, 608))
+    for i in range(loop):
+      try:
+        imgInfo = getImgInfo(pi)
+        ret = detect.run(self.yoloNet, imgInfo)
+        foundObjs = imgInfo['foundObjs']
+        print(i, 'imgInfo foundObjs -> ', foundObjs)
+        if ret is not None:
+          print('*****', ret, DIFFDECODES[ret])
+          self.yoloNet.drawDetectedObjects('Diff', imgInfo['img'], imgInfo['foundObjs'])
+        time.sleep(.5)
+      except Exception as e:
+        print('runMovement', e)
+
+  def runAllCamMovements(self):
+    for i in [0, 1]:
+      print(self.pis[i])
+      self.runCamMovement(i, loop=3)
+
+  def runSearchCamCache(self, timePattern="1600*.jpg", piId=1, day=0, classIds=[0,2]):
+    count = 0
+    pi = self.pis[piId]
+    piRepo = PiCamCacheRepo(pi['ip'], day=day, pattern=timePattern)
+    while(True):
+      imgInfo = piRepo.getImgInfo(rotate=pi['rotate'])
+      if (imgInfo is None):
+        break
+      objs = self.yoloNet.findDetectedObjects(imgInfo['img'])
+      if objs is not None:
+        print(imgInfo['timestamp'], 'imgInfo foundObjs -> ', objs)
+        for obj in objs:
+          if obj['classId'] in classIds:
+            self.yoloNet.drawDetectedObjects(imgInfo['timestamp'], imgInfo['img'], objs)
+            break
+      count = count + 1
+    print('Total', count, 'searched')
+
+  def app_main(self, methodName='runCamMovement'):
+    method = getattr(self, methodName)
+    method()
 
 import sys
 if __name__ == '__main__':
-  jbConf = json.load(open("jbconf.json"))
-  modelPath = jbConf["models"]["yolov3"]
-  yoloNet = Yolo(modelPath)
-  pis = json.load(open("../src/assets/pi-addr.json"))
-  # run(yoloNet, loadImgs(pis))
-  # runSearch(yoloNet, '192.168.0.110', day=1, pattern='1800*.jpg', rotate=False, classId=0)
-  # sys.exit()
-  for pi in pis:
-    print(pi)
-    runMovement(yoloNet, pi, loop=3)
+  homePiApp = HomePiApp()
+  if len(sys.argv) > 1:
+    homePiApp.app_main(sys.argv[1])
+  else:
+    homePiApp.app_main()
--- py/nono-main.py
+++ py/nono-main.py
@@ -1,56 +1,73 @@
 import time
-
+import json
 from Yolo import Yolo, DiffYolo, DIFFDECODES
 from nono_util import UrlSnapshot, getImgInfo, getImgInfosFromRepo
-
-def runMovement(yoloNet, u, p, channel=1, loop=3):
-  print('channel', channel)
-  detect = DiffYolo()
-  url = jbConf["nono"]['url']
-  urlSnapshot = UrlSnapshot(u, p)
-  for i in range(loop):
-    try:
-      imgInfo = getImgInfo(urlSnapshot, url, channel)
-      ret = detect.run(yoloNet, imgInfo)
-      foundObjs = imgInfo['foundObjs']
-      print(i, 'imgInfo foundObjs -> ', foundObjs)
-      if ret is not None:
-        print('*****', ret, DIFFDECODES[ret])
-        yoloNet.drawDetectedObjects('Diff', imgInfo['img'], imgInfo['foundObjs'])
-      time.sleep(.1)
-    except Exception as e:
-      print('ex', e)
-
-def runMovementFromRepo(yoloNet, channel=1):
-  print('channel', channel)
-  detect = DiffYolo()
-  imgInfos = getImgInfosFromRepo(channel)
-  for imgInfo in imgInfos:
-    try:
-      ret = detect.run(yoloNet, imgInfo)
-      foundObjs = imgInfo['foundObjs']
-      print('imgInfo foundObjs -> ', foundObjs)
-      if ret is not None:
-        print('*****', ret, DIFFDECODES[ret])
-        yoloNet.drawDetectedObjects('Diff', imgInfo['img'], imgInfo['foundObjs'])
-      time.sleep(.1)
-    except Exception as e:
-      print('ex', e)
-
 from nono_util import saveImgInfo, getSaveImgInfo
-def runCamCache(url, u, p, channel=1, loop=5, sleepSeconds=.5):
-  urlSnapshot = UrlSnapshot(u, p)
-  getSaveImgInfo(urlSnapshot, url, channel=channel)
 
-import sys
-import json
-if __name__ == '__main__':
-  if len(sys.argv) > 2:
+class NonoApp:
+  def __init__(self, u, p):
+    self.u = u
+    self.p = p
     jbConf = json.load(open("jbconf.json"))
     modelPath = jbConf["models"]["yolov3"]
-    yoloNet = Yolo(modelPath, confidence=.3, threshold=.2)
-    runCamCache(jbConf["nono"]['url'], sys.argv[1], sys.argv[2], channel=1)
-    # runMovementFromRepo(yoloNet, 1)
-    # sys.exit()
+    self.url = jbConf["nono"]['url']
+    self.yoloNet = Yolo(modelPath, confidence=.3, threshold=.2)
+
+  def runCamMovement(self, channel=1, loop=3):
+    detect = DiffYolo()
+    urlSnapshot = UrlSnapshot(self.u, self.p)
+    for i in range(loop):
+      try:
+        imgInfo = getImgInfo(urlSnapshot, self.url, channel)
+        ret = detect.run(self.yoloNet, imgInfo)
+        foundObjs = imgInfo['foundObjs']
+        print(i, 'imgInfo foundObjs -> ', foundObjs)
+        if ret is not None:
+          print('*****', ret, DIFFDECODES[ret])
+          self.yoloNet.drawDetectedObjects('Diff', imgInfo['img'], imgInfo['foundObjs'])
+        time.sleep(.1)
+      except Exception as e:
+        print('ex', e)
+
+  def runAllCamMovements(self):
     for ch in [1, 3, 0, 2]:
-      runMovement(yoloNet, sys.argv[1], sys.argv[2], channel=ch, loop=3)
+      print('channel', ch)
+      self.runCamMovement(channel=ch, loop=3)
+
+  def runCacheCam(self, channel=1, loop=5, sleepSeconds=.5):
+    urlSnapshot = UrlSnapshot(self.u, self.p)
+    getSaveImgInfo(urlSnapshot, self.url, channel=channel)
+
+  def runMovementFromRepo(self, channel=1):
+    detect = DiffYolo()
+    imgInfos = getImgInfosFromRepo(channel)
+    for imgInfo in imgInfos:
+      try:
+        ret = detect.run(self.yoloNet, imgInfo)
+        foundObjs = imgInfo['foundObjs']
+        print('imgInfo foundObjs -> ', foundObjs)
+        if ret is not None:
+          print('*****', ret, DIFFDECODES[ret])
+          self.yoloNet.drawDetectedObjects('Diff', imgInfo['img'], imgInfo['foundObjs'])
+        # time.sleep(.1)
+      except Exception as e:
+        print('runMovementFromRepo', e)
+
+  def app_main(self, methodName='runCamMovement'):
+    method = getattr(self, methodName)
+    method()
+
+def app_main(runMethod):
+  runMethod()
+
+import sys
+if __name__ == '__main__':
+  if len(sys.argv) > 2:
+    nonoApp = NonoApp(sys.argv[1], sys.argv[2])
+    # app_main(nonoApp.runAllCamMovements)
+    # app_main(nonoApp.runCacheCam)
+    # app_main(nonoApp.runMovementFromRepo)
+    if len(sys.argv) > 3:
+      nonoApp.app_main(sys.argv[3])
+    else:
+      nonoApp.app_main()
