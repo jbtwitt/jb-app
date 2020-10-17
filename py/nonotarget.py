2794058fd18f63d72552c81ded538aaf303c94ec
unknown <jbtwitt@outlook.com>
2020-10-17 08:42:25 -0400

    learn new use of list looping

--- py/nonotarget.py
+++ py/nonotarget.py
@@ -3,7 +3,7 @@ from yolotarget import findTarget
 
 IDX_CLASSIDS = 2
 CHANNEL_WATCHES = [
-  ['store', 0, [0]],
+  ['store', 0, [0, 56]],  # person, chair
   ['room', 1, [0]],
   ['driveway1', 2, [0, 2, 1, 3]],
   ['driveway2', 3, [0, 2, 1, 3]]  # person, car, bicycle, motorbike
@@ -17,6 +17,6 @@ def findNonoTarget(yoloNet, urlSnapshot, url, channel):
   objs, matches = findTarget(yoloNet, data, CHANNEL_WATCHES[channel][IDX_CLASSIDS])
   if objs is not None:
     nonopath.saveNonoImg(imgPath, timestamp, data)
-    print(imgPath, timestamp, objs, matches)
+    print(imgPath, timestamp, objs, [yoloNet.classLabel(id) for id in matches])
     return imgPath, timestamp, objs, matches
   return None, None, None, None
--- py/yolotarget.py
+++ py/yolotarget.py
@@ -3,12 +3,12 @@ import numpy as np
 import nonopath
 
 def matchClassIds(objs, targetClassIds):
-  matches = []
-  if objs is not None:
-    for classId, _, _ in objs:
-      if classId in targetClassIds:
-        matches.append(classId)
-  return matches
+  if objs is None:
+    return []
+  return [classId for classId, _, _ in objs for id in targetClassIds if classId == id]
+  # for classId, _, _ in objs:
+  #   if classId in targetClassIds:
+  #     matches.append(classId)
 
 def findTarget(yoloNet, imgdata, classIds):
   try:
@@ -18,8 +18,3 @@ def findTarget(yoloNet, imgdata, classIds):
   except Exception as e:
     print("findTarget error", e)
   return None, None
-
-# print([f for f in [1,2,3] if f >= 2])
-# t = [(1,[0],2)]
-# for a,b,c in t:
-#   print(a,b,c)
