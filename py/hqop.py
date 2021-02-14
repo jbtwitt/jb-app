ed7f91115c0f7a0cba435cec09f7ffeb6957f09f
jb <jbtwitt@outlook.com>
2021-02-13 20:31:44 -0500

    init hqop

---
+++ py/hqop.py
@@ -0,0 +1,46 @@
+# hq operation library
+
+DateFormat = "%Y-%m-%d"
+HqOpColumns = ['Symbol', 'HqType', 'Date', 'No', 'HqTypeChg', 'CCChg']
+
+def hq_addCols(df):
+  df['PrvClose'] = df.Close.shift(1)
+  df['PrvLow'] = df.Low.shift(1)
+  df['PrvHigh'] = df.High.shift(1)
+  df['PrvVolume'] = df.Volume.shift(1)
+  df['CCChg'] = (df.Close - df.PrvClose) / df.PrvClose
+  df['LLChg'] = (df.Low - df.PrvLow) / df.PrvClose
+  df['HHChg'] = (df.High - df.PrvHigh) / df.PrvClose
+  df['VVChg'] = df.Volume - df.PrvVolume
+  df['No'] = [df.shape[0] - df.index.get_loc(idx) - 1 for idx in df.index]
+
+def hq_llbcp(symbol, df, results, withinDays=2):
+  # Low Low but Close Positive
+  llbcp = df[(df.LLChg < 0) & (df.Close > df.PrvClose) & (df.No < withinDays)]
+  if len(llbcp) > 0:
+    for idx in llbcp.index:
+      results.append((
+        symbol, 'LLBCP{}'.format(withinDays), idx.strftime(DateFormat),
+        llbcp.No[idx], llbcp.LLChg[idx], llbcp.CCChg[idx]))
+
+def hq_hhbcn(symbol, df, results, withinDays=2):
+  # High Hight but Close Negative
+  hhbcn = df[(df.HHChg > 0) & (df.Close < df.PrvClose) & (df.No < withinDays)]
+  if len(hhbcn) > 0:
+    for idx in hhbcn.index:
+      results.append((
+        symbol, 'HHBCN{}'.format(withinDays), idx.strftime(DateFormat),
+        hhbcn.No[idx], hhbcn.HHChg[idx], hhbcn.CCChg[idx]))
+
+def hq_testVolume():
+  import pandas as pd
+  from hqscan import symbols, HqCsvRepo
+  for symbol in symbols:
+    df = pd.read_csv(HqCsvRepo.format(symbol), index_col=[0], parse_dates=True)
+    hq_addCols(df)
+    # last number days on volume grow 
+    # print(symbol, df.PrvVolume[df.No == 0], df.Volume[df.No == 0], df.VVChg[df.No == 0].sum())
+    print(symbol, df.VVChg[df.No < 10].sum())
+
+if __name__ == "__main__":
+  hq_testVolume()
--- py/hqscan.py
+++ py/hqscan.py
@@ -2,56 +2,38 @@ import pandas as pd
 from pandas_datareader import data as pdr
 from datetime import datetime, timedelta
 import numpy as np
+import hqop
 
-DateFormat = "%Y-%m-%d"
-HqRepo = "/home/jb/hq/download/{}.csv"
-HqResultRepo = "/home/jb/hq/"
-HqScanResult = HqResultRepo + "hqscan.csv"
-symbols = """VSTM,WATT,ATHX,OVID,SCPS,BCRX,AGEN,NNOX,NH,VTVT,XBIO,OSMT,BNGO,FOLD,HOTH,JAGX,JNCE,
-XM,SPCE,GEVO,BLNK"""
+HqRepo = "../src/assets/hqcsv/"
+HqCsvRepo = HqRepo + "download/{}.csv"
+HqScanResult = HqRepo + "hqscan.csv"
+symbols = """
+VSTM,WATT,ATHX,OVID,SCPS,BCRX,AGEN,NNOX,NH,VTVT,XBIO,OSMT,BNGO,FOLD,HOTH,JAGX,JNCE,
+XM,SPCE,GEVO,BLNK
+"""
 
 symbols = symbols.replace("\n", "").split(',')
 
 def hqdownload(symbols, startDate):
-  today = datetime.now().strftime(DateFormat)
+  today = datetime.now().strftime(hqop.DateFormat)
   for symbol in symbols:
     df = pdr.DataReader(symbol, data_source="yahoo", start=startDate, end=today)
-    df.to_csv(HqRepo.format(symbol))
-
-def hq_llbcp(symbol, df, results, withinDays=2):
-  # Low Low but Close Positive
-  llbcp = df[(df.LowSlope < 0) & (df.Close > df.PrvClose) & (df.No < withinDays)]
-  if len(llbcp) > 0:
-    for idx in llbcp.index:
-      results.append((symbol, 'LLBCP', idx.strftime(DateFormat), llbcp.No[idx]))
-
-def hq_hhbcn(symbol, df, results, withinDays=2):
-  # High Hight but Close Negative
-  hhbcn = df[(df.HighSlope > 0) & (df.Close < df.PrvClose) & (df.No < withinDays)]
-  if len(hhbcn) > 0:
-    for idx in hhbcn.index:
-      results.append((symbol, 'HHBCN', idx.strftime(DateFormat), hhbcn.No[idx]))
+    df.to_csv(HqCsvRepo.format(symbol))
 
 def hq_scan(symbols, withinDays=3):
   results = []
   for symbol in symbols:
-    df = pd.read_csv(HqRepo.format(symbol), index_col=[0], parse_dates=True)
-    df['PrvClose'] = df.Close.shift(1)
-    df['PrvLow'] = df.Low.shift(1)
-    df['PrvHigh'] = df.High.shift(1)
-    df['LowSlope'] = (df.Low - df.PrvLow) / df.PrvClose
-    df['HighSlope'] = (df.High - df.PrvHigh) / df.PrvClose
-    df['No'] = [df.shape[0] - df.index.get_loc(idx) - 1 for idx in df.index]
-    df.to_csv(HqRepo.format(symbol))
+    df = pd.read_csv(HqCsvRepo.format(symbol), index_col=[0], parse_dates=True)
+    hqop.hq_addCols(df)
 
     # begin scan
-    hq_llbcp(symbol, df, results)
-    hq_hhbcn(symbol, df, results)
+    hqop.hq_llbcp(symbol, df, results)
+    hqop.hq_hhbcn(symbol, df, results)
 
-  return pd.DataFrame(data=np.array(results), columns=['Symbol', 'HqType', 'Date', 'No'])
+  return pd.DataFrame(data=np.array(results), columns=hqop.HqOpColumns)
 
-def run(symbols, nDays=90):
-  startDate = (datetime.now() + timedelta(days=-nDays)).strftime(DateFormat)
+def run(symbols, nDays=120):
+  startDate = (datetime.now() + timedelta(days=-nDays)).strftime(hqop.DateFormat)
   hqdownload(symbols, startDate)
   results = hq_scan(symbols)
   results.to_csv(HqScanResult)
