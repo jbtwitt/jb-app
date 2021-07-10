6a04ac8a4b94f5d88b71d071454bb07e4306ad3a
jb <jbtwitt@outlook.com>
2021-07-10 12:03:00 -0400

    hqpdutil

--- py/hqpdutil.py
+++ py/hqpdutil.py
@@ -13,24 +13,17 @@ def straightLlDays(llDays):
   llDays = pd.DataFrame(data=llDays)
   llDays['NextNo'] = llDays.No.shift(1)
   llDays['NoStep'] = llDays.NextNo - llDays.No
-  llDays = llDays.reindex(index=llDays.index[::-1])   # reverse date order
-  print(llDays)
-  startNo = llDays.No[0]
-  stepNotEq1 = llDays[llDays.NoStep != 1]
-  # print(stepNotEq1)
-  endNo = stepNotEq1.No[0]
-  endNo = llDays[llDays.NoStep != 1].No[0]
-  # straightLlDays = llDays[(llDays.No >= startNo) & (llDays.No < stepNotEq1.No[0])]
-  print('startNo:', startNo, ' endNo:', endNo)
-  return startNo, endNo
+  llDays = llDays.reindex(index=llDays.index[::-1])     # reverse date order
+  return llDays.No[0], llDays[llDays.NoStep != 1].No[0] # return straightStart, straightEnd
 
 if __name__ == "__main__":
   for tick in ['BNGO', 'AVIR']:
     df = hqu.pdtick(tick)
     hqu.pdAddCols(df)
     llDays = pdLlDays(df, daysAgo=10)
+    straightStart, straightEnd = straightLlDays(llDays)
+    print(df[(df.No >= straightStart) & (df.No <= straightEnd)])
     # print(tick, 'straight LL days:', straightLlDays)
     # print('percent diff:', 100 *
     #   (llDays[llDays.No == 0].Close[0] - df[df.No == straightLlDays].Close[0]) /
     #   df[df.No == straightLlDays].Close[0], '%')
-    straightLlDays(llDays)
